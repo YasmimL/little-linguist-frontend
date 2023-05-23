cca66fc716a9d28f12e7e7c57c6c94f5593f6e55
yasmimnogueira <yasmim.nogueira@dellead.com>
2023-05-23 20:30:41 -0300

    Finalização do design da tela de atividades.

--- src/app/pages/activities/activities.component.ts
+++ src/app/pages/activities/activities.component.ts
@@ -3,8 +3,47 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-activities',
   templateUrl: './activities.component.html',
-  styleUrls: ['./activities.component.scss']
+  styleUrls: ['./activities.component.scss'],
 })
 export class ActivitiesComponent {
+  activities = [
+    {
+      src: 'assets/images/animals.png',
+      portugueseName: 'Animais',
+      englishName: 'Animals',
+      width: '12rem',
+    },
+    {
+      src: 'assets/images/colors.png',
+      portugueseName: 'Cores',
+      englishName: 'Colors',
+      width: '9rem',
+    },
+    {
+      src: 'assets/images/fruits.png',
+      portugueseName: 'Frutas',
+      englishName: 'Fruits',
+      width: '10rem',
+    },
+    {
+      src: 'assets/images/numbers.png',
+      portugueseName: 'Números',
+      englishName: 'Numbers',
+      width: '9rem',
+    },
+    {
+      src: 'assets/images/alphabet.png',
+      portugueseName: 'Alfabeto',
+      englishName: 'Alphabet',
+      width: '10rem',
+    },
+    {
+      src: 'assets/images/mathematical-symbols.png',
+      portugueseName: 'Símbolos Matemáticos',
+      englishName: 'Mathematical Symbols',
+      width: '10rem',
+    },
+  ];
 
+  constructor() {}
 }
